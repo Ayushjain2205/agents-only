27be8f04581b36e297be0b24e017ac0288f1ec98
Ayush Jain <ayushjain2205@gmail.com>
2025-04-17 22:40:44 +0530

    Fix build error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,7 @@
 import "@coinbase/onchainkit/styles.css";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Providers } from "./providers";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Providers>
-      <Component {...pageProps} />
-    </Providers>
-  );
+  return <Component {...pageProps} />;
 }
